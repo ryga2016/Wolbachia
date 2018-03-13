165950e06154f5bc3192fc6136795c445613d0ed
johnnyp117 <35807919+johnnyp117@users.noreply.github.com>
2018-03-12 22:30:26 -0400

    base level code for GUI

---
+++ TkinterGUIa12-3-18.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from tkinter.filedialog  import askopenfilename
+from tkinter.filedialog import asksaveasfile 
+# save as a pyw to double click and run?
+#import wolbachiaOut.py
+#import wolhacbiaRead.py
+
+class wolbachiaGUI():
+
+# Technically, everything occurs here. By simply running this file, 
+# or calling this class, the gui will run.         
+    def __init__(self):
+        global master
+        global e1 
+        global e2 
+        global e3
+        global e4
+        e3 = StringVar()
+        e4 = StringVar()
+        master = Tk()
+# This turns the X button in the corner into a failsafe device.        
+        master.protocol('WM_DELETE_WINDOW', wolbachiaGUI.on_exit)
+        Label(master, text="inFile").grid(row=0)
+        Label(master, text="outFile").grid(row=1)
+#I'm unsure if the last part is needed; will investigate making the width variable to len(input)        
+        e1 = Entry(master, width=100, textvariable=e3)
+        e2 = Entry(master, width=100, textvariable=e4)
+           
+        e1.grid(row=0, column=1)
+        e2.grid(row=1, column=1)
+ # Creates buttons, doesn't run the methods imbedded within until clicked due to the lack of '()'   
+        Button(master, text='...', command= wolbachiaGUI.fileI).grid(row=0,column=2, sticky=W, pady=4)
+        Button(master, text='...', command= wolbachiaGUI.fileO).grid(row=1,column=2, sticky=W, pady=4)     
+        Button(master, text='Run', command= wolbachiaGUI.wolbachiaRead).grid(row=3, column=2, sticky=W, pady=4)
+        
+        master.mainloop( )
+        
+#Dummy method    
+    def wolbachiaRead():
+        print(e1.get(),'\n',e2.get())
+        
+# Button actions activate!
+    def fileI():
+        e3 = askopenfilename(initialdir='~')
+        e1.insert(0, e3)
+               
+    def fileO():
+        e4 = asksaveasfile(defaultextension=".txt")
+        e4.close()
+        e2.insert(0, e4.name)
+        
+
+# Fail-safe method for actually closing the program. W/o, kernal err occurs. 
+    @classmethod    
+    def on_exit(self):
+        master.destroy()
+        master.quit()
+        sys.exit
+        
+            
+wolbachiaGUI()
+   
